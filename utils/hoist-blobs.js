00563a1d43455ca05132a4bb486c6c51fe0b9187
AdarshPandya0 <adarshp@meditab.com>
2026-06-01 13:47:20 +0530

    Report merge fixes ,Test folder cleanup changes and gchat webhook setup

---
+++ utils/hoist-blobs.js
@@ -0,0 +1,21 @@
+import fs from 'fs';
+import path from 'path';
+
+const blobDir = path.resolve('blob-report');
+
+let counter = 1;
+if (fs.existsSync(blobDir)) {
+    fs.readdirSync(blobDir).forEach(item => {
+        const itemPath = path.join(blobDir, item);
+        // If it's one of our sub-folders (like blob-report-1)
+        if (fs.statSync(itemPath).isDirectory()) {
+            fs.readdirSync(itemPath).forEach(file => {
+                if (file.endsWith('.zip')) {
+                    // Move the zip file up to the root blob-report folder and rename it safely
+                    fs.renameSync(path.join(itemPath, file), path.join(blobDir, `shard-${counter++}.zip`));
+                }
+            });
+        }
+    });
+    console.log('All blobs successfully hoisted for merging');
+}
